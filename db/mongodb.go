eff8943f7aabcada215559c84ac555a9e4ade79e
Hequn Zhang <heqzha@gmail.com>
2016-11-28 12:07:38 +0800

    Fix few mongodb bugs and add test codes

--- db/mongodb.go
+++ db/mongodb.go
@@ -12,20 +12,23 @@ import(
 type MongoDBHandler struct{
 	se *mgo.Session
 	url string
-	db string
 }
 
-func MongoDBNewHandler(username, password, dbName string, url ...string)(*MongoDBHandler, error){
+type BsonM bson.M
+
+func MongoDBNewHandler(username, password, db string, url ...string)(*MongoDBHandler, error){
 	jurl := strings.Join(url, ",")
 	var furl string
-	if username != "" {
-		furl := fmt.Sprintf("mongodb://%s:%s@%s/%s", username, password, jurl, dbName)
-	}else if dbName != ""{
-		furl := fmt.Sprintf("mongodb://%s/%s", jurl, dbName)
-	}else{
-		dbName = "default"
-		furl := fmt.Sprintf("mongodb://%s/%s", jurl, dbName)
+	if db == ""{
+		db = "default"
 	}
+
+	if username != "" {
+		furl = fmt.Sprintf("mongodb://%s:%s@%s/%s", username, password, jurl, db)
+	}else {
+		furl = fmt.Sprintf("mongodb://%s/%s", jurl, db)
+	}
+
 	se, err := mgo.Dial(furl)
 	if err != nil{
 		return nil, err
@@ -33,7 +36,6 @@ func MongoDBNewHandler(username, password, dbName string, url ...string)(*MongoD
 	return &MongoDBHandler{
 		se: se,
 		url: furl,
-		db: dbName,
 	}, nil
 }
 
@@ -45,26 +47,28 @@ func (h *MongoDBHandler)Renew()(*MongoDBHandler, error){
 	return &MongoDBHandler{
 		se: se,
 		url: h.url,
-		db: h.db,
 	}, nil
 }
 
 func (h *MongoDBHandler)Close(){
-	h.se.Close
-	mgo.Monotonic
-}
-
-func (h *MongoDBHandler)SelectDB(dbName string){
-	h.db = dbName
+	h.se.Close()
 }
 
 func (h *MongoDBHandler)NewID()string{
-	return string(bson.NewObjectId())
+	return bson.NewObjectId().Hex()
 }
 
-func (h *MongoDBHandler)EnsureIndex(cName string, unique, dropDups, background, sparse bool, keys ...string) error{
-	c := h.se.DB(h.db).C(cName)
-	return c.EnsureIndex(Index{
+func (h *MongoDBHandler)CheckID(id string)bool{
+	return bson.IsObjectIdHex(id)
+}
+
+func (h *MongoDBHandler)ToObjectID(id string)bson.ObjectId{
+	return bson.ObjectIdHex(id)
+}
+
+func (h *MongoDBHandler)EnsureIndex(db, cName string, unique, dropDups, background, sparse bool, keys ...string) error{
+	c := h.se.DB(db).C(cName)
+	return c.EnsureIndex(mgo.Index{
 		Key: keys,
 		Unique: unique,
 		DropDups: dropDups,
@@ -73,13 +77,13 @@ func (h *MongoDBHandler)EnsureIndex(cName string, unique, dropDups, background, 
 	})
 }
 
-func (h *MongoDBHandler)DropIndex(cName string, keys ...string) error{
-	c := h.se.DB(h.db).C(cName)
-	return c.DropIndex(keys)
+func (h *MongoDBHandler)DropIndex(db, cName string, keys ...string) error{
+	c := h.se.DB(db).C(cName)
+	return c.DropIndex(keys...)
 }
 
-func (h *MongoDBHandler)Indexes(cName string)([]map[string]interface{}, error){
-	c := h.se.DB(h.db).C(cName)
+func (h *MongoDBHandler)Indexes(db, cName string)([]map[string]interface{}, error){
+	c := h.se.DB(db).C(cName)
 	indexes, err := c.Indexes()
 	if err != nil{
 		return nil, err
@@ -87,56 +91,60 @@ func (h *MongoDBHandler)Indexes(cName string)([]map[string]interface{}, error){
 
 	m := []map[string]interface{}{}
 	for _, index := range indexes{
-		mIdx := structs.Map(indexes)
+		mIdx := structs.Map(index)
 		m = append(m, mIdx)
 	}
-	return m
+	return m, nil
 }
 
-func (h *MongoDBHandler)find(cName string, selector map[string]interface{}) (*mgo.Query){
-	c := h.se.DB(h.db).C(cName)
-	return c.Find(bson.M(selector))
+func (h *MongoDBHandler)find(db, cName string, selector BsonM) (*mgo.Query){
+	c := h.se.DB(db).C(cName)
+	return c.Find(selector)
 }
 
-func (h *MongoDBHandler)findByID(cName string, id string) (*mgo.Query){
-	c := h.se.DB(h.db).C(cName)
+func (h *MongoDBHandler)findByID(db, cName string, id string) (*mgo.Query){
+	c := h.se.DB(db).C(cName)
 	return c.FindId(bson.ObjectIdHex(id))
 }
 
-func (h *MongoDBHandler)FindAll(cName string, offset, limit int, sort map[string]interface{}, results interface{}) error{
+func (h *MongoDBHandler)FindAll(db, cName string, offset, limit int, sort []string, results interface{}) error{
 	if len(sort) != 0{
-		return h.find(cName, nil).Sort(sort).Skip(offset).Limit(limit).All(&results)
+		return h.find(db, cName, nil).Sort(sort...).Skip(offset).Limit(limit).All(results)
 	}
-	return h.find(cName, nil).Skip(offset).Limit(limit).All(&results)
+	return h.find(db, cName, nil).Skip(offset).Limit(limit).All(results)
 }
 
-func (h *MongoDBHandler)FindOne(cName string, offset, limit int, sort map[string]interface{}, result interface{}) error{
+func (h *MongoDBHandler)FindOne(db, cName string, selector BsonM, offset, limit int, sort []string, result interface{}) error{
 	if len(sort) != 0{
-		return h.find(cName, nil).Sort(sort).Skip(offset).Limit(limit).One(&result)
+		return h.find(db, cName, selector).Sort(sort...).Skip(offset).Limit(limit).One(result)
 	}
-	return h.find(cName, nil).Skip(offset).Limit(limit).One(&result)
+	return h.find(db, cName, selector).Skip(offset).Limit(limit).One(result)
 }
 
-func (h *MongoDBHandler)FindByID(cName, id string, result interface{}) error{
-	return h.findByID(cName, id).One(&result)
+func (h *MongoDBHandler)Find(db, cName string, selector BsonM, result interface{}) error{
+	return h.find(db, cName, selector).One(result)
 }
 
-func (h *MongoDBHandler)Insert(cName string, cObjects...interface{}) error{
-	c := h.se.DB(h.db).C(cName)
+func (h *MongoDBHandler)FindByID(db, cName, id string, result interface{}) error{
+	return h.findByID(db, cName, id).One(result)
+}
+
+func (h *MongoDBHandler)Insert(db, cName string, cObjects...interface{}) error{
+	c := h.se.DB(db).C(cName)
 	return c.Insert(cObjects...)
 }
 
-func (h *MongoDBHandler)Upsert(cName string, selector map[string]interface{}, cObject interface{}) (string, error){
-	c := h.se.DB(h.db).C(cName)
-	info, err := c.Upsert(bson.M(selector), cObject)
+func (h *MongoDBHandler)Upsert(db, cName string, selector BsonM, cObject interface{}) (int, error){
+	c := h.se.DB(db).C(cName)
+	info, err := c.Upsert(selector, cObject)
 	if err != nil{
-		return "", err
+		return 0, err
 	}
-	return string(info.UpsertedId.(bson.ObjectId)), nil
+	return info.Matched, nil
 }
 
-func (h *MongoDBHandler)UpsertedId(cName, id string, cObject interface{})(string, error){
-	c := h.se.DB(h.db).C(cName)
+func (h *MongoDBHandler)UpsertedId(db, cName, id string, cObject interface{})(string, error){
+	c := h.se.DB(db).C(cName)
 	info, err := c.UpsertId(bson.ObjectIdHex(id), cObject)
 	if err != nil{
 		return "", err
@@ -144,21 +152,21 @@ func (h *MongoDBHandler)UpsertedId(cName, id string, cObject interface{})(string
 	return string(info.UpsertedId.(bson.ObjectId)), nil
 }
 
-func (h *MongoDBHandler)Remove(cName string, selector map[string]interface{}) error{
-	c := h.se.DB(h.db).C(cName)
-	return c.Remove(bson.M(selector))
+func (h *MongoDBHandler)Remove(db, cName string, selector BsonM) error{
+	c := h.se.DB(db).C(cName)
+	return c.Remove(selector)
 }
 
-func (h *MongoDBHandler)RemoveAll(cName string, selector map[string]interface{}) (int, error){
-	c := h.se.DB(h.db).C(cName)
-	info, err := c.RemoveAll(bson.M(selector))
+func (h *MongoDBHandler)RemoveAll(db, cName string, selector BsonM) (int, error){
+	c := h.se.DB(db).C(cName)
+	info, err := c.RemoveAll(selector)
 	if err != nil{
 		return 0, err
 	}
-	return info.Removed(), err
+	return info.Removed, nil
 }
 
-func (h *MongoDBHandler)RemoveByID(cName, id string) error{
-	c := h.se.DB(h.db).C(cName)
+func (h *MongoDBHandler)RemoveByID(db, cName, id string) error{
+	c := h.se.DB(db).C(cName)
 	return c.RemoveId(bson.ObjectIdHex(id))
 }
---
+++ test/mongodb_test.go
@@ -0,0 +1,90 @@
+package test
+
+import(
+	"fmt"
+	"testing"
+	"github.com/heqzha/goutils/date"
+	"github.com/heqzha/goutils/db"
+)
+
+type ATestData struct{
+	ID string `bson:"_id"`
+	Name string `bson:"name"`
+	Age int32 `bson:"age"`
+	CreatedTs int64 `bson:"created_ts"`
+}
+
+func TestMongoDBHandler(t *testing.T){
+	dbName := "test"
+	tName := "a_test_data"
+	h, err := db.MongoDBNewHandler("","", dbName, "127.0.0.1:27017")
+	if err != nil{
+		t.Error(err.Error())
+		return
+	}
+	defer h.Close()
+
+	data := ATestData{
+		ID: h.NewID(),
+		Name: "TestName",
+		Age: 10,
+		CreatedTs: date.DateNowSecond(),
+	}
+	if err := h.Insert(dbName, tName, data); err!=nil{
+		t.Error(err.Error())
+		return
+	}
+	t.Log(fmt.Sprintf("Insert data: %v\n", data))
+
+	result := ATestData{}
+	if err := h.Find(dbName, tName, db.BsonM{
+		"name": "TestName",
+	}, &result); err != nil{
+		t.Error(err.Error())
+		return
+	}
+	t.Log(fmt.Sprintf("Find result: %v\n", result))
+
+	data.Age++
+	numUpsert, err := h.Upsert(dbName, tName, db.BsonM{
+		"_id":data.ID,
+	}, data)
+	if err != nil{
+		t.Error(err.Error())
+		return
+	}
+	t.Log(fmt.Sprintf("Upsert num: %d\n", numUpsert))
+
+
+	if err := h.EnsureIndex(dbName, tName, false, false, false, false, "name", "age"); err != nil{
+		t.Error(err.Error())
+		return
+	}
+	indexes, err := h.Indexes(dbName, tName)
+	if err != nil{
+		t.Error(err.Error())
+		return
+	}
+	t.Log(fmt.Sprintf("Indexes: %v\n", indexes))
+
+	if err := h.DropIndex(dbName, tName, "name", "age"); err != nil{
+		t.Error(err.Error())
+		return
+	}
+
+	if err := h.Remove(dbName, tName, db.BsonM{
+		"age": db.BsonM{
+			"$gt":10,
+		},
+	}); err != nil{
+		t.Error(err.Error())
+		return
+	}
+
+	numRemoved, err := h.RemoveAll(dbName, tName, nil)
+	if err != nil{
+		t.Error(err.Error())
+		return
+	}
+	t.Log(fmt.Sprintf("Removed num: %d\n", numRemoved))
+}
