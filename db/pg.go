ba4d627324e6006d1acd3467979e6c4926d867e5
Hequn Zhang <heqzha@gmail.com>
2017-07-19 10:19:56 +0800

    Update pg

--- db/pg.go
+++ db/pg.go
@@ -1,12 +1,8 @@
 package db
 
 import (
-	"errors"
 	"fmt"
-	"log"
 	"strconv"
-	"strings"
-	"sync"
 	"time"
 
 	xormcore "github.com/go-xorm/core"
@@ -15,26 +11,16 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var (
-	pgEngineMaster = &PGEngine{}
-	pgEngineMux    = sync.RWMutex{}
-)
-
-type PGSession struct {
-	*xorm.Session
-}
-
 type PGEngine struct {
 	*xorm.Engine
-	dsn    string
-	Status bool
+	dsn string
 }
 
-func PGConfig(engine *PGEngine,
-	user, password, dbName, host, port string, ssl bool,
-	level xormcore.LogLevel, showSQL bool) error {
-	var err error
+type PGMasterEngine struct {
+	*PGEngine
+}
 
+func (pg *PGMasterEngine) Init(user, password, dbName, host, port string, ssl bool, level xormcore.LogLevel, showSQL bool) error {
 	dsn := fmt.Sprintf(
 		"user=%s dbname=%s host=%s port=%s",
 		user,
@@ -48,191 +34,60 @@ func PGConfig(engine *PGEngine,
 			password,
 		)
 	}
-
 	if ssl {
 		dsn += " sslmode=enable"
 	} else {
 		dsn += " sslmode=disable"
 	}
-
-	if engine.Engine, err = xorm.NewEngine("postgres", dsn); err != nil {
+	pg = &PGMasterEngine{}
+	engine, err := xorm.NewEngine("postgres", dsn)
+	if err != nil {
 		return err
 	}
+	pg.Engine = engine
+	pg.dsn = dsn
+	pg.SetMaxOpenConns(100)
+	pg.SetMaxIdleConns(50)
+	pg.Logger().SetLevel(level)
+	pg.ShowSQL(showSQL)
 
-	engine.dsn = dsn
-	engine.SetMaxOpenConns(100)
-	engine.SetMaxIdleConns(50)
-	engine.Logger().SetLevel(level)
-	engine.ShowSQL(showSQL)
-
-	if err = engine.Ping(); err != nil {
-		engine.Status = false
+	if err := pg.Ping(); err != nil {
 		return err
 	}
-	engine.Status = true
 	return nil
 }
 
-func PGRunCheckMasterStatus(interval time.Duration) {
+func (pg *PGMasterEngine) SyncModels(models ...interface{}) error {
+	return pg.Sync2(models...)
+}
+
+func (pg *PGMasterEngine) EnableEngineStatusChecker(t time.Duration) {
 	ccc.TaskRunPeriodic(func() time.Duration {
-		pgCheckDBEngineStatus(pgEngineMaster)
-		return interval
-	}, "PGRunCheckMasterStatus", interval*time.Second)
-}
-
-func pgCheckDBEngineStatus(engine *PGEngine) {
-	err := engine.Ping()
-	if err != nil {
-		pgEngineMux.RLock()
-		if engine.Status {
-			pgEngineMux.RUnlock()
-			pgEngineMux.Lock()
-			engine.Status = false
-			pgEngineMux.Unlock()
-		} else {
-			pgEngineMux.RUnlock()
+		if err := pg.Ping(); err != nil {
+			fmt.Println("Cannot connect to %s: %s", pg.dsn, err.Error())
 		}
-		log.Printf("pgCheckDBEngineStatus dsn: %s, error: %s\n", engine.dsn, err.Error())
-		return
-	}
-
-	pgEngineMux.RLock()
-	if !engine.Status {
-		pgEngineMux.RUnlock()
-		pgEngineMux.Lock()
-		engine.Status = true
-		pgEngineMux.Unlock()
-	} else {
-		pgEngineMux.RUnlock()
-	}
+		return t
+	}, "PGRunCheckMasterStatus", time.Second)
 }
 
-func PGNewMasterSession() *PGSession {
-	ms := new(PGSession)
-	ms.Session = pgEngineMaster.NewSession()
-
-	return ms
+func (pg *PGMasterEngine) GetAutoCloseSession() *xorm.Session {
+	s := pg.NewSession()
+	s.IsAutoClose = true
+	return s
 }
 
-func PGGetEngine(master, sync bool) (*PGEngine, error) {
-	pgEngineMux.RLock()
-	defer pgEngineMux.RUnlock()
-
-	if master {
-		if pgEngineMaster.Status {
-			return pgEngineMaster, nil
-		}
-
-		return nil, fmt.Errorf("DB engine not avaliable")
-	}
-
-	// if sync {} //TODO
-	return nil, fmt.Errorf("DB engine not avaliable")
-}
-
-func pgNewMasterAutoCloseSession() *PGSession {
-	return pgNewAutoCloseSession(true, false)
-}
-
-func pgNewAutoCloseSession(master, sync bool) *PGSession {
-	engine, err := PGGetEngine(master, sync)
+func (pg *PGMasterEngine) NewSequence(sequence string) (int64, error) {
+	results, err := pg.Query("select nextval(?) as next", sequence)
 	if err != nil {
-		engine = pgEngineMaster
+		return 0, fmt.Errorf("Generate sequence %s error: %s", sequence, err.Error())
+	} else if results == nil || len(results) == 0 {
+		return 0, fmt.Errorf("Generate sequence %s error: results is empty", sequence)
 	}
 
-	ms := new(PGSession)
-	ms.Session = engine.NewSession()
-	ms.IsAutoClose = true
-
-	return ms
-}
-
-type DBModel interface {
-	TableName() string
-}
-
-func InsertRow(s *PGSession, m DBModel) (err error) {
-	if s == nil {
-		s = pgNewMasterAutoCloseSession()
-	}
-	_, err = s.AllCols().InsertOne(m)
-
-	if err != nil && strings.Index(err.Error(), "duplicate key") >= 0 {
-		err = errors.New("DB model duplicated")
-	}
-
-	return
-}
-
-func InsertMultiRows(s *PGSession, m []interface{}) (err error) {
-	var ms *PGSession
-
-	if s == nil {
-		ms = PGNewMasterSession()
-		defer ms.Close()
-		if err = ms.Begin(); err != nil {
-			return err
-		}
-	} else {
-		ms = s
-	}
-
-	_, err = ms.AllCols().Insert(m...)
-	if s == nil {
-		if err != nil {
-			ms.Rollback()
-		} else {
-			err = ms.Commit()
-		}
-	}
-
-	if err != nil && strings.Index(err.Error(), "duplicate key") >= 0 {
-		err = errors.New("DB model duplicated")
-	}
-
-	return
-}
-
-type UniqueDBModel interface {
-	TableName() string
-	UniqueCond() (string, []interface{})
-}
-
-func UpdateDBModel(s *PGSession, m UniqueDBModel) (err error) {
-	whereStr, whereArgs := m.UniqueCond()
-	if s == nil {
-		s = pgNewMasterAutoCloseSession()
-	}
-
-	_, err = s.AllCols().Where(whereStr, whereArgs...).Update(m)
-	if err != nil && strings.Index(err.Error(), "duplicate key") >= 0 {
-		err = errors.New("DB model duplicated")
-	}
-
-	return
-}
-
-func DeleteDBModel(s *PGSession, m UniqueDBModel) (err error) {
-	whereStr, whereArgs := m.UniqueCond()
-
-	if s == nil {
-		s = pgNewMasterAutoCloseSession()
-	}
-
-	_, err = s.Where(whereStr, whereArgs...).Delete(m)
-	return
-}
-
-func pgGenSequenceValue(sequence string) (int64, error) {
-	results, err := pgEngineMaster.Query("select nextval(?) as next", sequence)
+	result, err := strconv.ParseInt(string(results[0]["next"]), 10, 64)
 	if err != nil {
-		return 0, fmt.Errorf("Generate %s sequence error: %s", sequence, err.Error())
+		return 0, fmt.Errorf("Generate sequence %s error: %s", sequence, err.Error())
 	}
 
-	id, err := strconv.ParseInt(string(results[0]["next"]), 10, 64)
-	if err != nil {
-		return 0, fmt.Errorf("Generate %s sequence error: %s", sequence, err.Error())
-	}
-
-	return id, nil
+	return result, nil
 }
