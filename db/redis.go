1f4783a69ebbfb809b87d50a0a42ab0fb6ef719b
heqzha <heqzha@gmail.com>
2017-12-22 12:48:07 +0800

    Add new redis handler

---
+++ db/redis.go
@@ -0,0 +1,271 @@
+package db
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+type RedisHandler struct {
+	Pool *redis.Pool
+}
+
+func (h *RedisHandler) Init(addr string) {
+	h.Close()
+	h.Pool = &redis.Pool{
+		MaxIdle:     5,
+		IdleTimeout: 120 * time.Second,
+
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", addr,
+				redis.DialConnectTimeout(time.Duration(100)*time.Millisecond),
+				redis.DialReadTimeout(time.Duration(100)*time.Millisecond),
+				redis.DialWriteTimeout(time.Duration(100)*time.Millisecond))
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func (h *RedisHandler) Close() {
+	if h.Pool != nil {
+		h.Pool.Close()
+		h.Pool = nil
+	}
+}
+
+func (h *RedisHandler) Ping() error {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	_, err := redis.String(conn.Do("PING"))
+	if err != nil {
+		return fmt.Errorf("cannot 'PING' db: %v", err)
+	}
+	return nil
+}
+
+func (h *RedisHandler) Get(key string) ([]byte, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	var data []byte
+	data, err := redis.Bytes(conn.Do("GET", key))
+	if err != nil {
+		return data, fmt.Errorf("error getting key %s: %v", key, err)
+	}
+	return data, err
+}
+
+func (h *RedisHandler) Set(key string, value []byte) error {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("SET", key, value)
+	if err != nil {
+		v := string(value)
+		if len(v) > 15 {
+			v = v[0:12] + "..."
+		}
+		return fmt.Errorf("error setting key %s to %s: %v", key, v, err)
+	}
+	return err
+}
+
+func (h *RedisHandler) Exists(key string) (bool, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	ok, err := redis.Bool(conn.Do("EXISTS", key))
+	if err != nil {
+		return ok, fmt.Errorf("error checking if key %s exists: %v", key, err)
+	}
+	return ok, err
+}
+
+func (h *RedisHandler) Delete(key string) error {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("DEL", key)
+	return err
+}
+
+func (h *RedisHandler) GetKeys(pattern string) ([]string, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	iter := 0
+	keys := []string{}
+	for {
+		arr, err := redis.Values(conn.Do("SCAN", iter, "MATCH", pattern))
+		if err != nil {
+			return keys, fmt.Errorf("error retrieving '%s' keys", pattern)
+		}
+
+		iter, _ = redis.Int(arr[0], nil)
+		k, _ := redis.Strings(arr[1], nil)
+		keys = append(keys, k...)
+
+		if iter == 0 {
+			break
+		}
+	}
+
+	return keys, nil
+}
+
+func (h *RedisHandler) Incr(key string) (int, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	return redis.Int(conn.Do("INCR", key))
+}
+
+func (h *RedisHandler) Zadd(key string, value []byte, score int64) error {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+	_, err := conn.Do("ZADD", key, score, value)
+	if err != nil {
+		v := string(value)
+		if len(v) > 15 {
+			v = v[0:12] + "..."
+		}
+		return fmt.Errorf("err zadd keyy %s to %s: err  %v", key, v, conn.Err())
+	}
+	return err
+}
+
+func (h *RedisHandler) Zcard(key string) (int, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	res, err := conn.Do("ZCARD", key)
+	if err != nil {
+		return -1, fmt.Errorf("error zcard key %s %v", key, err)
+	}
+	return int(res.(int64)), nil
+}
+
+func (h *RedisHandler) Zrange(key string, offset, limit int) ([][2]string, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	res, err := conn.Do("ZRANGE", key, offset, offset*limit+limit, "WITHSCORES")
+	if err != nil {
+		return nil, fmt.Errorf("error zrange key %s %v", key, err)
+	}
+	results := [][2]string{}
+	ress := res.([]interface{})
+	for idx, _ := range ress {
+		if idx&0x1 == 0 {
+			results = append(results, [2]string{string(ress[idx+1].([]byte)), string(ress[idx].([]byte))})
+		}
+	}
+	return results, nil
+}
+
+func (h *RedisHandler) Zrangebyscore(key string, min, max int64, offset, limit int) ([][2]string, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	res, err := conn.Do("ZRANGEBYSCORE", key, "("+strconv.FormatInt(min, 10), strconv.FormatInt(max, 10), "WITHSCORES", "LIMIT", offset, limit)
+	if err != nil {
+		return nil, fmt.Errorf("error zrangebyscore key %s %v", key, err)
+	}
+	results := [][2]string{}
+	ress := res.([]interface{})
+	for idx, _ := range ress {
+		if idx&0x1 == 0 {
+			results = append(results, [2]string{string(ress[idx+1].([]byte)), string(ress[idx].([]byte))})
+		}
+	}
+	return results, nil
+}
+
+func (h *RedisHandler) Zrevrangebyscore(key string, min, max int64, offset, limit int) ([][2]string, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	res, err := conn.Do("ZREVRANGEBYSCORE", key, strconv.FormatInt(max, 10), "("+strconv.FormatInt(min, 10), "WITHSCORES", "LIMIT", offset, limit)
+	if err != nil {
+		return nil, fmt.Errorf("error zrangebyscore key %s %v", key, err)
+	}
+	results := [][2]string{}
+	ress := res.([]interface{})
+	for idx, _ := range ress {
+		if idx&0x1 == 0 {
+			results = append(results, [2]string{string(ress[idx+1].([]byte)), string(ress[idx].([]byte))})
+		}
+	}
+	return results, nil
+}
+
+func (h *RedisHandler) Zcount(key string, min, max int64) (int, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	res, err := conn.Do("ZCOUNT", key, "("+strconv.FormatInt(min, 10), strconv.FormatInt(max, 10))
+	if err != nil {
+		return -1, fmt.Errorf("error zcount key %s %v", key, err)
+	}
+	return int(res.(int64)), nil
+}
+
+func (h *RedisHandler) Zremrangebyrank(key string, start, stop int64) error {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("ZREMRANGEBYRANK", key, start, stop)
+	if err != nil {
+		return fmt.Errorf("error zremrangebyrank key %s :%v", key, err)
+	}
+	return err
+}
+
+func (h *RedisHandler) Expire(key string, seconds int64) error {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	_, err := conn.Do("EXPIRE", key, seconds)
+	if err != nil {
+		return fmt.Errorf("error expire key %s to %d: %v", key, seconds, err)
+	}
+	return err
+}
+
+func (h *RedisHandler) Ttl(key string) (int, error) {
+
+	conn := h.Pool.Get()
+	defer conn.Close()
+
+	res, err := conn.Do("TTL", key)
+	if err != nil {
+		return -1, fmt.Errorf("error ttl key %s %v", key, err)
+	}
+	return int(res.(int64)), nil
+}
---
+++ test/redis_test.go
@@ -0,0 +1,198 @@
+package test
+
+import "testing"
+import "github.com/heqzha/goutils/db"
+import "strconv"
+
+import "time"
+
+func TestRedisHandlerInit(t *testing.T) {
+	handler := &db.RedisHandler{}
+	defer handler.Close()
+	handler.Init("127.0.0.1:9600")
+	t.Log(handler.Pool)
+
+}
+
+func TestRedisHandlerPing(t *testing.T) {
+	handler := &db.RedisHandler{}
+	defer handler.Close()
+	handler.Init("127.0.0.1:9600")
+
+	if err := handler.Ping(); err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log("Ping")
+}
+
+func TestRedisHandlerSetGet(t *testing.T) {
+	handler := &db.RedisHandler{}
+	defer handler.Close()
+	handler.Init("127.0.0.1:9600")
+	key := "test_set"
+	exist, err := handler.Exists(key)
+	if err != nil {
+		t.Error(err)
+		return
+	} else if exist {
+		if err := handler.Delete(key); err != nil {
+			t.Error(err)
+			return
+		}
+		t.Logf("exist %s, deleted!", key)
+	}
+
+	if err := handler.Set(key, []byte("123")); err != nil {
+		t.Error(err)
+		return
+	}
+	val, err := handler.Get(key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(string(val))
+	handler.Delete(key)
+}
+
+func TestRedisHandlerGetKeys(t *testing.T) {
+	handler := &db.RedisHandler{}
+	defer handler.Close()
+	handler.Init("127.0.0.1:9600")
+	key := "test_set"
+
+	if err := handler.Set(key, []byte("123")); err != nil {
+		t.Error(err)
+		return
+	}
+
+	keys, err := handler.GetKeys("test_*")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(keys)
+
+	handler.Delete(key)
+}
+
+func TestRedisHandlerIncr(t *testing.T) {
+	handler := &db.RedisHandler{}
+	defer handler.Close()
+	handler.Init("127.0.0.1:9600")
+	key := "test_incr"
+	cnter, err := handler.Incr(key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(cnter)
+}
+
+func TestRedisHandlerZ(t *testing.T) {
+	handler := &db.RedisHandler{}
+	defer handler.Close()
+	handler.Init("127.0.0.1:9600")
+	key := "test_z"
+
+	for index := 0; index < 3; index++ {
+		if err := handler.Zadd(key, []byte(strconv.Itoa(index)), int64(index)); err != nil {
+			t.Error(err)
+			return
+		}
+	}
+	card, err := handler.Zcard(key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(card)
+
+	r, err := handler.Zrange(key, 0, 10)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(r)
+
+	r, err = handler.Zrangebyscore(key, -1, 1, 0, 10)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(r)
+
+	r, err = handler.Zrevrangebyscore(key, -1, 1, 0, 10)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(r)
+
+	cnt, err := handler.Zcount(key, -1, 1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(cnt)
+
+	if err := handler.Zremrangebyrank(key, 0, 1); err != nil {
+		t.Error(err)
+		return
+	}
+	r, err = handler.Zrange(key, 0, 10)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(r)
+}
+
+func TestRedisHandlerExpire(t *testing.T) {
+	handler := &db.RedisHandler{}
+	defer handler.Close()
+	handler.Init("127.0.0.1:9600")
+	key := "test_set"
+
+	if err := handler.Set(key, []byte("123")); err != nil {
+		t.Error(err)
+		return
+	}
+	if err := handler.Expire(key, 3); err != nil {
+		t.Error(err)
+		return
+	}
+	ttl, err := handler.Ttl(key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(ttl)
+	val, err := handler.Get(key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(string(val))
+
+	// for exist, _ := handler.Exists(key); exist; {
+	// 	fmt.Println("exist")
+	// 	time.Sleep(1 * time.Second)
+	// }
+	time.Sleep(5 * time.Second)
+	exist, err := handler.Exists(key)
+	if err != nil {
+		t.Error(err)
+		return
+	} else if !exist {
+		t.Log("not exist")
+		return
+	}
+	val, err = handler.Get(key)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(string(val))
+}
